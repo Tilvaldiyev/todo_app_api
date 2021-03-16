90a5f9e84aa91c91a4ae335bcc97a9888f80aa28
Tilvaldiyev <tilvaldiyev@gmail.com>
2021-03-16 23:36:19 +0600

    implemented clean architecture

--- cmd/main.go
+++ cmd/main.go
@@ -4,13 +4,17 @@ import (
 	"log"
 	"todo_app_api"
 	"todo_app_api/pkg/handler"
+	"todo_app_api/pkg/repository"
+	"todo_app_api/pkg/service"
 )
 
 func main() {
-	handler := new(handler.Handler)
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
 
 	srv := new(todo.Server)
-	if err := srv.Run("8000", handler.InitRoutes()); err != nil {
+	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
 		log.Fatalf("Error ocured while running http server: %s", err.Error())
 	}
 }
--- pkg/handler/handler.go
+++ pkg/handler/handler.go
@@ -1,10 +1,19 @@
 package handler
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"todo_app_api/pkg/service"
+)
 
 type Handler struct {
-
+	services *service.Service
 }
+
+func NewHandler(services *service.Service) *Handler {
+	return &Handler{services: services}
+}
+
+
 
 func (h *Handler) InitRoutes() *gin.Engine {
 	router := gin.New()
