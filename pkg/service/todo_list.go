8df2bb120579c797a95328d6f5eea57f76291020
Tilvaldiyev <tilvaldiyev@gmail.com>
2021-03-18 17:05:03 +0600

    Delete list

--- pkg/service/todo_list.go
+++ pkg/service/todo_list.go
@@ -26,3 +26,7 @@ func (s *TodoListService) GetAll(userId int) ([]todo.TodoList, error) {
 func (s *TodoListService) GetById(userId, listId int) (todo.TodoList, error) {
 	return s.repo.GetById(userId, listId)
 }
+
+func (s *TodoListService) Delete(userId, listId int) error {
+	return s.repo.Delete(userId, listId)
+}
